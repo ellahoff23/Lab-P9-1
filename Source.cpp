8a337510c5a6d810b36db2caf58b9031e8e4ba5c
Ella Hoffman <e-hoffman.2@onu.edu>
2024-03-28 17:51:42 -0400

    Can now display only the first and last element

--- Source.cpp
+++ Source.cpp
@@ -11,30 +11,34 @@ int main() {
     srand(time(nullptr));
 
     for (int i = 0; i < size; i++) {
-        randomNumbers[i] = rand() % 9000 + 1000;  
+        randomNumbers[i] = rand() % 9000 + 1000;
     }
 
-    cout << "Elements in order of appearance: ";
+    cout << "The random integers: ";
     for (int i = 0; i < size; i++) {
         cout << randomNumbers[i] << " ";
     }
 
-    cout << "\nElements at even indices:        ";
+    cout << "\nEven indices:        ";
     for (int i = 0; i < size; i += 2) {
         cout << randomNumbers[i] << " ";
     }
 
-    cout << "\nEven elements:                   ";
+    cout << "\nEven values:         ";
     for (int i = 0; i < size; i++) {
         if (randomNumbers[i] % 2 == 0) {
             cout << randomNumbers[i] << " ";
         }
     }
 
-    cout << "\nElements in reverse order:       ";
+    cout << "\nReversed order:      ";
     for (int i = size - 1; i >= 0; i--) {
         cout << randomNumbers[i] << " ";
     }
+
+    cout << "\nFirst and last:      ";
+    cout << randomNumbers[0] << " ";
+    cout << randomNumbers[size - 1] << endl;
 
     return 0;
 }
