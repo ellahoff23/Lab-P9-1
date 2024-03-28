589b2c3493112d33f63fa7b3683c85fcf489ddf6
Ella Hoffman <e-hoffman.2@onu.edu>
2024-03-28 17:37:10 -0400

    Can now display all elements in reverse order

--- Source.cpp
+++ Source.cpp
@@ -14,21 +14,26 @@ int main() {
         randomNumbers[i] = rand() % 9000 + 1000;  
     }
 
-    cout << "Elements in order of appearance:" << endl;
+    cout << "Elements in order of appearance: ";
     for (int i = 0; i < size; i++) {
-        cout << randomNumbers[i] << endl;
+        cout << randomNumbers[i] << " ";
     }
 
-    cout << "\nElements at even indices:" << endl;
+    cout << "\nElements at even indices:        ";
     for (int i = 0; i < size; i += 2) {
-        cout << randomNumbers[i] << endl;
+        cout << randomNumbers[i] << " ";
     }
 
-    cout << "\nEven elements:" << endl;
+    cout << "\nEven elements:                   ";
     for (int i = 0; i < size; i++) {
         if (randomNumbers[i] % 2 == 0) {
-            cout << randomNumbers[i] << endl;
+            cout << randomNumbers[i] << " ";
         }
+    }
+
+    cout << "\nElements in reverse order:       ";
+    for (int i = size - 1; i >= 0; i--) {
+        cout << randomNumbers[i] << " ";
     }
 
     return 0;
