00fee43a2eb8f9e6a03a37ba6f016399b1f6fa76
Ella Hoffman <e-hoffman.2@onu.edu>
2024-03-28 17:19:01 -0400

    Can now display every element at an even index

--- Source.cpp
+++ Source.cpp
@@ -14,7 +14,13 @@ int main() {
         randomNumbers[i] = rand() % 9000 + 1000;  
     }
 
+    cout << "Elements in order of appearance:" << endl;
     for (int i = 0; i < size; i++) {
+        cout << randomNumbers[i] << endl;
+    }
+
+    cout << "\nElements at even indices:" << endl;
+    for (int i = 0; i < size; i += 2) {
         cout << randomNumbers[i] << endl;
     }
 
