3eb14b5d5a03ee2c57f9dec6d39719c451b579f4
Ella Hoffman <e-hoffman.2@onu.edu>
2024-03-28 17:11:24 -0400

    Can now display every element in order of appearance in the array

--- Source.cpp
+++ Source.cpp
@@ -1,1 +1,22 @@
- 
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main() {
+    const int size = 10;
+    int randomNumbers[size];
+
+    srand(time(nullptr));
+
+    for (int i = 0; i < size; i++) {
+        randomNumbers[i] = rand() % 9000 + 1000;  
+    }
+
+    for (int i = 0; i < size; i++) {
+        cout << randomNumbers[i] << endl;
+    }
+
+    return 0;
+}
